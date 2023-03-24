8983a9efc8a6537d575a5925f881a6b008dba0c3
kyrylo <kpogrebnyak.28@gmail.com>
2023-03-24 11:59:21 +0100

    Classes

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+class Library:
+    def __init__(self, books, borrower):
+        self.books = books
+        self.borrower = borrower
 
+class Book(Library):
+    def __init__(self, title, author, isbn, availability):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.availability = availability
+
+    def info()
+
+class Borrower(Library):
+    def __init__(self, name, id, borrowed_books):
+        self.name = name
+        self.id = id
+        self.borrowed_books = borrowed_books
+
+    def info()
