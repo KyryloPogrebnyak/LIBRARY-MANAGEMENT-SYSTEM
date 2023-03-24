6e77c033d04d19c68b5dd79d24337830bd729ff3
Spencer-OBrien <spencerobrien@live.com>
2023-03-24 14:22:20 +0100

    Spencer's first commit

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 LIBRARY MANAGEMENT SYSTEM 
-   - book class (title, author, isbn, availability, .info())
-   - borrower class (name, id, borrowed_books, .info())
-   - library class (books, borrowers, .add_book(), .add_borrower(), .loan_book(), .return_book(), .find_book(), .find_borrower())
+```py
+   book class (title, author, isbn, availability, .info())
+   
+   borrower class (name, id, borrowed_books, .info())
+   
+   library class (books, borrowers, .add_book(), .add_borrower(), .loan_book(), .return_book(), .find_book(), .find_borrower())
+```
--- main.py
+++ main.py
@@ -1,21 +1,43 @@
-class Library:
-    def __init__(self, books, borrower):
-        self.books = books
-        self.borrower = borrower
-
-class Book(Library):
+class Book():
     def __init__(self, title, author, isbn, availability):
         self.title = title
         self.author = author
         self.isbn = isbn
         self.availability = availability
 
-    def info()
+    def info(self):
+        pass
 
-class Borrower(Library):
-    def __init__(self, name, id, borrowed_books):
+class Borrower():
+    def __init__(self, name, id):
         self.name = name
         self.id = id
-        self.borrowed_books = borrowed_books
+        self.borrowed_books = []
 
-    def info()
+    def info(self):
+        pass
+        
+
+
+
+class Library(Book, Borrower):
+    def __init__(self):
+        self.books = ["The Hitchhiker's Guide to the Galaxy", "Harry Potter", "The Hobbit", "Ready Player One"]
+        self.borrower = []
+
+    def find_books(self):
+        search = input("Search the book you are looking for: ")
+        if search in self.books:
+            return f"'{search}' is available."
+        else:
+            return f"'{search}' is unavailable."
+
+    def find_borrower(self):
+        search = input("Search for User name: ")
+        if search.lower() in self.borrower.lower():
+            return f"Welcome {self.borrower}."
+        else:
+            add_borrower() ### needs to be implemented 
+
+b = Library()
+print(b.find_books())
